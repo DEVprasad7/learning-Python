75b4edd2f3e3054bc58cde02a7b5a147dc2c931f
DEVprasad7 <maskar.prasad.comp@kgce.edu.in>
2024-10-19 22:39:16 +0530

    miner changes

--- conditions and loops/guess_game.py
+++ conditions and loops/guess_game.py
@@ -10,15 +10,15 @@ def play_game():
     
     if user_input == 1:
         user_guess = int(input("Pick a number from 0-9: "))
-        system_number = random.randint(0, 9)  # Generate random number in 0-9
+        system_number = random.randint(0, 9)  
     elif user_input == 2:
         user_guess = int(input("Pick a number from 0-100: "))
-        system_number = random.randint(0, 100)  # Generate random number in 0-100
+        system_number = random.randint(0, 100)  
     else:
         print("Invalid input.")
-        return  # Exit the function if input is invalid
+        return  
 
-    # Compare user's guess with the system-generated number
+    
     if user_guess == system_number:
         print(f"Congratulations! You guessed it right. The number was {system_number}.")
     else:
@@ -31,13 +31,12 @@ def main():
         while True:
             play_game()
             
-            # Ask if they want to play again (just for the guessing part)
             retry = int(input("Do you want to guess again? (1 = Yes, 0 = No): "))
             if retry == 0:
                 print("Thank you for playing! Goodbye.")
-                break  # Exit the loop if user chooses not to guess again
+                break  
     else:
         print("Okay, bye!")
 
-# Run the game
+
 main()
