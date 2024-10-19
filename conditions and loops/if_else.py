9f95f101fc591b1630268c26544146008440946b
DEVprasad7 <maskar.prasad.comp@kgce.edu.in>
2024-10-19 16:09:42 +0530

    conditions in python

---
+++ conditions and loops/if_else.py
@@ -0,0 +1,11 @@
+#user inputs for comparisons
+A = int(input("Enter an integer X :"))
+B = int(input("Enter an integer Y :"))
+
+#if_else condition 
+if A > B:
+    print("X is greater")
+elif A < B:
+    print("Y is greater")
+else:
+    print("X and Y1 are equal")
