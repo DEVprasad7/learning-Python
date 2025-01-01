7496a09ce7d77a11f724b24d55e4899a128b86d5
DEVprasad7 <maskar.prasad.comp@kgce.edu.in>
2025-01-01 19:12:04 +0530

    queue in python

--- Functions and variables/queue.py
+++ Functions and variables/queue.py
@@ -12,6 +12,16 @@ def pop_item(queue):
     else:
         print("Popped item is: ", queue.pop(0))
 
+def pop_item_specific(queue, item):
+    if not queue:
+        print("Queue is empty")
+    else:
+        item = int(input("Enter the item to pop: "))
+        if item in queue:
+            queue.remove(item)
+        else:
+            print("Item not found in the queue")
+
 def view_queue(queue):
     print(queue)
 
@@ -20,14 +30,16 @@ def main():
     max_length = int(input("Enter the max length of the queue: "))
     item = 0
     while True:
-        choice = int(input("Enter 1 to push, 2 to pop, 3 to view queue and 4 to quit: "))
+        choice = int(input("Enter 1 to push, 2 to pop, 3 to pop specific item, 4 to view queue and 5 to quit: "))
         if choice == 1:
             push_item(queue, item, max_length)
         elif choice == 2:
             pop_item(queue)
         elif choice == 3:
-            view_queue(queue)
+            pop_item_specific(queue, item)
         elif choice == 4:
+            view_queue(queue)
+        elif choice == 5:
             break
         else:
             print("Invalid choice")
