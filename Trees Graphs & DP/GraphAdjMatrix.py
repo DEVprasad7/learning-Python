80a5ec9a4e45068bb94d1651fb73ff5dcc886a76
DEVprasad7 <prasad.maskar7@gmail.com>
2026-01-19 21:53:30 +0530

    Graph Starting

---
+++ Trees Graphs & DP/GraphAdjMatrix.py
@@ -0,0 +1,27 @@
+class Graph:
+    def __init__(self, size: int):
+        self.mat = [ [0]*size for _ in range(size) ]
+        self.size = size
+        
+    def add_edge(self, src: int, dest: int, weight: int = 1, directed = False):
+        if (0 <= src <= self.size and 0 <= dest <= self.size):
+            if directed == True:
+                self.mat[src][dest] = weight
+            else:
+                self.mat[src][dest] = weight
+                self.mat[dest][src] = weight
+                
+        else:
+            print("Invalid edge Parameter")
+            
+    def print(self):
+        for row in self.mat:
+            print(" ".join(map(str, row)))
+        
+        
+G = Graph(3)
+G.add_edge(0, 1)
+G.add_edge(0, 2)
+G.add_edge(1, 2)
+G.print()
+
